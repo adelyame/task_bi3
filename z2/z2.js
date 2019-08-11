364d3126b01428c191c35d679d609075ee6ae4cc
Adelya <adelya19992989@gmail.com>
2019-08-11 18:14:05 +0500

    Седьмой комит, второе задание

---
+++ z2/z2.js
@@ -0,0 +1,13 @@
+function check(){
+    let fibonacciNumber = document.getElementById('fibonacciNumber');
+    let primeNumber = document.getElementById('primeNumber');
+
+    if (){
+        
+    }
+}
+let answer = prompt('Введите количество чисел');
+function func(n) {
+    return n <= 1 ? n : func(n - 1) + func(n - 2);
+  }
+  console.log(func(9 ));
