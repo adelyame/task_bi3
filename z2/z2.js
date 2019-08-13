c208c051a16c414b4ff2de4d642fe197dbf11e59
Adelya <adelya19992989@gmail.com>
2019-08-13 18:49:57 +0500

    Восьмой комит второе задание

--- z2/z2.js
+++ z2/z2.js
@@ -1,13 +1,29 @@
-function check(){
+/*function check(){
     let fibonacciNumber = document.getElementById('fibonacciNumber');
     let primeNumber = document.getElementById('primeNumber');
 
     if (){
         
     }
+}*/
+function func(a) {
+
+    var result = [0, 1];
+    for (var i = 2; i <a; i++) {
+        result[i] =result[i-1] + result[i-2];
+
+    }return result;
 }
-let answer = prompt('Введите количество чисел');
-function func(n) {
-    return n <= 1 ? n : func(n - 1) + func(n - 2);
-  }
-  console.log(func(9 ));
+function fibonacci(){
+    var answer = +prompt('Введите количество чисел');
+    if (Number.isInteger(answer)){
+        alert(func(answer)); 
+
+    }
+    else{
+        alert('Введите число!');
+    }
+}
+
+
+   
